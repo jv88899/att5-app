e2d83defb6db3b007d6af0886b6fc5f752d5fd00
jv88899 <jv88899@gmail.com>
2019-08-07 17:05:36 -0500

    all time starting five button now returns data from server

--- src/components/App.js
+++ src/components/App.js
@@ -62,59 +62,19 @@ class App extends Component {
     })
   }
 
-  selectAllTimeStartingFive = () => {
-    const { players } = this.state
-    let allTimeStartingFive = []
-    let pgPER = 0
-    let sgPER = 0
-    let sfPER = 0
-    let pfPER = 0
-    let cPER = 0
-    let topPG = ''
-    let topSG = ''
-    let topSF = ''
-    let topPF = ''
-    let topC = ''
+  selectAllTimeStartingFive = async () => {
+    let response = await fetch('/api/v3/players/all-time')
+    const body = await response.json()
 
-    for (let i = 0; i < players.length; i++) {
-      if (players[i].primaryPosition === 'PG') {
-        if (players[i].careerPER > pgPER) {
-          pgPER = players[i].careerPER
-          topPG = players[i]
-        }
-      } else if (players[i].primaryPosition === 'SG') {
-        if (players[i].careerPER > sgPER) {
-          sgPER = players[i].careerPER
-          topSG = players[i]
-        }
-      } else if (players[i].primaryPosition === 'SF') {
-        if (players[i].careerPER > sfPER) {
-          sfPER = players[i].careerPER
-          topSF = players[i]
-        }
-      } else if (players[i].primaryPosition === 'PF') {
-        if (players[i].careerPER > pfPER) {
-          pfPER = players[i].careerPER
-          topPF = players[i]
-        }
-      } else if (players[i].primaryPosition === 'C') {
-        if (players[i].careerPER > cPER) {
-          cPER = players[i].careerPER
-          topC = players[i]
-        }
-      }
+    if (response.status !== 200) {
+      throw Error(body.message)
     }
 
-    allTimeStartingFive.push(topPG)
-    allTimeStartingFive.push(topSG)
-    allTimeStartingFive.push(topSF)
-    allTimeStartingFive.push(topPF)
-    allTimeStartingFive.push(topC)
-
     this.setState({
-      selectedPlayers: allTimeStartingFive,
+      selectedPlayers: body.data.players,
       selectedPosition: 'all-time'
     })
+
   }
 
   calculateScore = player => {
