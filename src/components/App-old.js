d2f918bd2b4afef4477e7e1128850a2ac10dad52
jv88899 <jv88899@gmail.com>
2019-11-16 16:47:25 -0600

    added html

---
+++ src/components/App-old.js
@@ -0,0 +1,308 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import AllTimeStartingFive from './AllTimeStartingFive/AllTimeStartingFive'
+import CriteriaForm from './CriteriaForm/CriteriaForm'
+import MultiPlayerCompare from './MultiPlayerCompare/MultiPlayerCompare'
+import PlayerBattle from './PlayerBattle/PlayerBattle'
+import PlayerCards from './PlayerCards/PlayerCards'
+import PlayerSearch from './PlayerSearch/PlayerSearch'
+import PositionMenu from './PositionMenu/PositionMenu'
+import ScoreInformationModal from './ScoreInformationModal/ScoreInformationModal'
+import './App.css'
+
+class App extends Component {
+  state = {
+    players: [],
+    selectedPosition: 'All',
+    selectedPlayers: [],
+    selectedPlayer: null,
+    allTimeStartingFive: [],
+    criteria: {
+      per: 1,
+      championships: 1,
+      ppg: 1,
+      mvp: 1,
+      allNBA: 1
+    },
+    scoreInformationModalVisible: false,
+    playerOne: null,
+    playerTwo: null,
+    searchCriteria: '',
+    playersToCompare: []
+  }
+
+  componentDidMount = () => {
+    this.getInitialPlayers()
+      .then( res => this.setState({
+        selectedPlayers: res.data.players,
+        players: res.data.players,
+      }))
+      .catch( err => console.log(err))
+  }
+
+  getInitialPlayers = async () => {
+    const response = await fetch('/api/v3/players/All')
+    const body = await response.json()
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+
+    return body
+  }
+
+  selectNewPosition = async newPosition => {
+    const { criteria, searchCriteria } = this.state
+    const response = await fetch('/api/v3/players/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        criteria,
+        selectedPosition: newPosition,
+        searchCriteria
+      })
+    })
+
+    const body = await response.json()
+
+    this.setState({
+      selectedPlayers: body.players,
+      selectedPosition: newPosition
+    })
+  }
+
+  selectAllTimeStartingFive = async () => {
+    const { criteria, searchCriteria } = this.state
+    const response = await fetch('/api/v3/players/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        selectedPosition: 'all-time',
+        criteria,
+        searchCriteria
+      })
+    })
+    const body = await response.json()
+    const newPlayers = body.players
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+
+    this.setState({
+      selectedPlayers: newPlayers,
+      selectedPosition: 'all-time'
+    })
+  }
+
+  handleCriteriaFormSubmit = async newCriteria => {
+    const { selectedPosition, searchCriteria } = this.state
+    const response = await fetch('/api/v3/players', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        criteria: newCriteria,
+        selectedPosition: selectedPosition,
+        searchCriteria: searchCriteria
+      })
+    })
+    const body = await response.json()
+    const newPlayers = body.players
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+
+    this.setState({
+      selectedPlayers: newPlayers,
+      criteria: newCriteria
+    })
+  }
+
+  handleSelectScoreInformation = selectedPlayer => {
+    this.setState({
+      selectedPlayer,
+      scoreInformationModalVisible: !this.state.scoreInformationModalVisible
+    })
+  }
+
+  handleCloseSelectScoreInformation = () => {
+    this.setState({ scoreInformationModalVisible: !this.state.scoreInformationModalVisible })
+  }
+
+  handlePlayerInputSubmit = async (player, playerNumber) => {
+    const response = await fetch('/api/v3/battle', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        newCriteria: this.state.criteria,
+        player
+      })
+    })
+
+    const body = await response.json()
+    const selectedPlayer = await body.player
+    console.log(selectedPlayer)
+
+    if (playerNumber === 'First Player') {
+      this.setState({ playerOne: selectedPlayer })
+    } else if (playerNumber === 'Second Player') {
+      this.setState({ playerTwo: selectedPlayer })
+    }
+  }
+
+  handlePlayerSearchChange = async searchTerm => {
+    const response = await fetch('/api/v3/search', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        newCriteria: this.state.criteria,
+        searchTerm,
+        selectedPosition: this.state.selectedPosition
+      })
+    })
+
+    const body = await response.json()
+    const searchResults = await body.searchResults
+    console.log('search results are ', searchResults)
+
+    this.setState({
+      selectedPlayers: searchResults,
+      searchCriteria: searchTerm,
+      selectedPosition: this.state.selectedPosition
+    })
+  }
+
+  addPlayerToCompare = (playerId) => {
+    const { playersToCompare } = this.state
+    this.setState({ playersToCompare: playersToCompare.concat(playerId) })
+  }
+
+  removePlayerToCompare = (playerId) => {
+    const { playersToCompare } = this.state
+    this.setState({ playersToCompare: playersToCompare.filter( id => id !== playerId) })
+  }
+
+  clearPlayersToCompare = () => {
+    this.setState({ playersToCompare: [] })
+  }
+
+  render() {
+    const { 
+      selectedPosition,
+      selectedPlayers,
+      selectedPlayer,
+      scoreInformationModalVisible,
+      playerOne,
+      playerTwo,
+      playersToCompare
+    } = this.state
+    return (
+      <Router>
+        <div className="app">
+          <div className="app-header">
+            <div className="app-header-wrapper">
+              <h1>All Time Starting 5</h1>
+              <p>The best starting 5 of all time, according to you.</p>
+              <CriteriaForm
+                handleCriteriaFormSubmit={this.handleCriteriaFormSubmit}
+              />
+              <div className="navigation">
+                <span>
+                  <Link to="/">All Time Starting Five</Link>
+                </span>
+                {/*
+                <span>
+                  <Link to="/battle">Battle</Link>
+                </span>
+                */}
+                {
+                  this.state.playersToCompare.length > 1 &&
+                <span>
+                    <Link to="/results">Compare Players</Link>
+                </span>
+                }
+              </div>
+              <div className="app-menu-container">
+                <div className="position-menu">
+                  <PositionMenu
+                    selectNewPosition={this.selectNewPosition}
+                    currentPosition={selectedPosition}
+                  />
+                  <AllTimeStartingFive
+                    currentPosition={selectedPosition}
+                    selectAllTimeStartingFive={this.selectAllTimeStartingFive}
+                  />
+                </div>
+                <div className="search-container">
+                  <PlayerSearch
+                    handlePlayerSearchChange={this.handlePlayerSearchChange}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <Route
+            path='/'
+            exact={true}
+            render={ (props) => <PlayerCards {...props}
+                players={selectedPlayers}
+                calculateScore={this.calculateScore}
+                handleSelectScoreInformation={this.handleSelectScoreInformation}
+                addPlayerToCompare={this.addPlayerToCompare}
+                removePlayerToCompare={this.removePlayerToCompare}
+              />
+            }
+          />
+          {/*
+          <Route
+            path='/battle'
+            exact={true}
+            render={ props => <PlayerBattle {...props}
+                handleSubmit={this.handlePlayerInputSubmit}
+                playerOne={playerOne}
+                playerTwo={playerTwo}
+              />
+            }
+          />
+          */}
+          <Route
+            path='/results'
+            exact={true}
+            render={ props => <MultiPlayerCompare {...props}
+                handleSelectScoreInformation={this.handleSelectScoreInformation}
+                addPlayerToCompare={this.addPlayerToCompare}
+                playersToCompare={playersToCompare}
+                clearPlayersToCompare={this.clearPlayersToCompare}
+                selectedPlayers={selectedPlayers}
+                removePlayerToCompare={this.removePlayerToCompare}
+              />
+            }
+          />
+          <ScoreInformationModal
+            visible={scoreInformationModalVisible}
+            hideModal={this.handleCloseSelectScoreInformation}
+            selectedPlayer={selectedPlayer}
+          />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,173 @@
+import React, { Component } from 'react'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+import './header.css'
+
+class Header extends Component {
+    render() {
+        let headerCriteriaMenuClassess = 'header__criteria__menu'
+        return (
+            <header className="header">
+                <nav className="nav">
+                    <div className="flex-container">
+                        <div className="nav__brand">
+                            <div className="nav__brand__text">
+                                <div className="nav__brand__text__letters">
+                                    <span>A</span>
+                                    <span>T</span>
+                                    <span>S</span>
+                                    <span>F</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="nav__menu">
+                            <ul className="nav__menu__links">
+                                <li>Link 1</li>
+                                <li>Tutorial</li>
+                                <li>About</li>
+                                <li>Donate</li>
+                            </ul>
+                        </div>
+                        <div className="nav__hamburger">
+                            <div className="nav__hamburger__bar"></div>
+                            <div className="nav__hamburger__bar"></div>
+                            <div className="nav__hamburger__bar"></div>
+                        </div>
+                    </div>
+                </nav>
+                <div className="header__about">
+                    <h1>All Time Starting Five</h1>
+                    <p>The best players of all time, according to you</p>
+                </div>
+                <div className="header__criteria">
+                    <React.Fragment>
+                        <div className="header__criteria__title">
+                            <h2>Current Criteria</h2>
+                        </div>
+                        <div className="header__criteria__summary">
+                        </div>
+                    </React.Fragment>
+                    <div className={headerCriteriaMenuClassess}>
+                        <div className="header__criteria__slider__group">
+                            <div className="header__slider__label">
+                                PPG
+                            </div>
+                            <div className="header__slider">
+                                <Slider
+                                    defaultValue={1}
+                                    min={1}
+                                    max={9}
+                                    step={1}
+                                /> 
+                            </div>
+                            <div className="header__slider_value">
+                                3
+                            </div>
+                        </div>
+                        <div className="header__criteria__slider__group">
+                            <div className="header__slider__label">
+                                PER
+                            </div>
+                            <div className="header__slider">
+                                <Slider
+                                    defaultValue={1}
+                                    min={1}
+                                    max={9}
+                                    step={1}
+                                />
+                            </div>
+                            <div className="header__slider__value">
+                                3
+                            </div>
+                        </div>
+                        <div className="header__criteria__slider__group">
+                            <div className="header__slider__label">
+                                CHA
+                            </div>
+                            <div className="header__slider">
+                                <Slider
+                                    defaultValue={1}
+                                    min={1}
+                                    max={9}
+                                    step={1}
+                                />
+                            </div>
+                            <div className="header__slider__value">
+                                3
+                            </div>
+                        </div>
+                        <div className="header__criteria__slider__group">
+                            <div className="header__slider__label">
+                                NBA
+                            </div>
+                            <div className="header__slider">
+                                <Slider
+                                    defaultValue={1}
+                                    min={1}
+                                    max={9}
+                                    step={1}
+                                />
+                            </div>
+                            <div className="header__slider__value">
+                                3
+                            </div>
+                        </div>
+                        <div className="header__criteria__slider__group">
+                            <div className="header__slider__label">
+                                MVP
+                            </div>
+                            <div className="header__slider">
+                                <Slider
+                                    defaultValue={1}
+                                    min={1}
+                                    max={9}
+                                    step={1}
+                                />
+                            </div>
+                            <div className="header__slider__value">
+                                3
+                            </div>
+                        </div>
+                    </div>
+                    <div className="header__criteria__button">
+                        {
+                            false ?
+                            <div className="header__criteria__button__container">
+                                <button
+                                    className="header__criteria__button__set"
+                                >
+                                    Change Criteria
+                                </button>
+                                <button
+                                    className="header__criteria__button__close"
+                                >
+                                    Close Menu
+                                </button>
+                            </div> :
+                            <button
+                            >
+                                View Criteria
+                            </button>
+                        }
+                    </div>
+                </div>
+                <div className="header__search">
+                        <form>
+                            <label>Search for player</label>
+                            <div className="header__search__group">
+                                <input
+                                    type="text"
+                                />
+                                <button
+                                >
+                                    Search
+                                </button>
+                            </div>
+                        </form>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header
